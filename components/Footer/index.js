1f98b1f4fd9e348cf004484965597259808fad75
Manu <manutorres1996@gmail.com>
2022-01-13 22:32:42 +0100

    feat:(Theme Context Creation: Footer)

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import Link from "next/link";
 import Home from "components/Icons/Home";
 import Search from "components/Icons/Search";
 import Create from "components/Icons/Create";
 import styles from "styles/Footer.module.css";
+import { ThemeContext } from "../../hooks/themeContext";
 
 export default function Footer() {
+  const { toggle: darkMode } = useContext(ThemeContext);
+
   return (
-    <nav className={styles.footer}>
+    <nav
+      className={darkMode ? `${styles.footer} ${styles.dark}` : styles.footer}
+    >
       <Link href="/home">
         <a>
           <Home width="28" height="28" />
