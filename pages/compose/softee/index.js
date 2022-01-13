1fc29a6ff149c0091ff85f656cdf1e47ebe52ecc
Manu <manutorres1996@gmail.com>
2022-01-13 22:38:57 +0100

    feat:(Theme: Compose/Softee && Hastags)

--- pages/compose/softee/index.js
+++ pages/compose/softee/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Button from "components/Button";
 import useUser from "hooks/useUser";
 import styles from "styles/ComposeSoftee.module.css";
@@ -8,10 +8,17 @@ import Head from "next/head";
 import Avatar from "components/Avatar";
 import Header from "components/Header";
 import Footer from "components/Footer";
+import { ThemeContext } from "hooks/themeContext";
 
 export default function ComposeSoftee() {
   const user = useUser();
+  const { toggle: darkMode } = useContext(ThemeContext);
   const [Message, setMessage] = useState("");
+  const [hastagOne, setHastagOne] = useState(null);
+  const [hastagTwo, setHastagTwo] = useState(null);
+  const [hastagThree, setHastagThree] = useState(null);
+  const [HastagValue, setHastags] = useState([]);
+  const [isTextAreaDisabled, setIsTextAreaDisabled] = useState(false);
 
   const COMPOSE_STATES = {
     USER_NOT_KNOWN: "user_undefined",
@@ -52,13 +59,50 @@ export default function ComposeSoftee() {
     }
   }, [Task]);
 
+  useEffect(() => {
+    if (hastagOne && hastagTwo && hastagThree) {
+      setIsTextAreaDisabled(true);
+    } else {
+      setIsTextAreaDisabled(false);
+    }
+  }, [hastagOne, hastagTwo, hastagThree]);
+
   const handleChange = (event) => {
     const { value } = event.target;
     setMessage(value);
   };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  const handleChangeHastags = (e) => {
+    const { value } = e.target;
+    if (value) {
+      setHastags(value);
+      // console.log(e);
+    }
+    console.log("value", value);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 32) {
+      if (!hastagOne) {
+        console.log("Writing H1");
+        setHastags("");
+        return setHastagOne(HastagValue);
+      }
+      if (!hastagTwo) {
+        console.log("Writing H2");
+        setHastags("");
+        return setHastagTwo(HastagValue);
+      }
+      if (!hastagThree) {
+        console.log("Writing H3");
+        setHastags("");
+        return setHastagThree(HastagValue);
+      }
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     setStatus("loading");
     try {
       const response = await addSoftee({
@@ -102,31 +146,55 @@ export default function ComposeSoftee() {
     }
   };
 
+  const textAreaStyles = `${styles.textarea}
+  ${Drag === DRAG_IMAGE_STATES.DRAG_OVER && styles.textareaDraggedIn}`;
+
   return (
     <>
       <Head>
         <title>Crear un Softee / Softter</title>
       </Head>
       <Header location="Post softee" />
-      <section className={styles.composeSofteeWrapper}>
+      <section
+        className={
+          darkMode
+            ? `${styles.composeSofteeWrapper} ${styles.darkMode}`
+            : styles.composeSofteeWrapper
+        }
+      >
         {user && (
           <section className={styles.avatar}>
             <Avatar src={user.avatar} />
           </section>
         )}
         <form className={styles.form} onSubmit={handleSubmit}>
-          <textarea
+          <SoffteeTextArea
             onDragEnter={handleDragEnter}
             onDragLeave={handleDragLeave}
             onDrop={handleDrop}
             onChange={handleChange}
-            placeholder="¿Qué esta pasando?"
-            className={`${styles.textarea}
-            ${
-              Drag === DRAG_IMAGE_STATES.DRAG_OVER && styles.textareaDraggedIn
-            }`}
+            darkMode={darkMode}
             value={Message}
-          ></textarea>
+            textAreaStyles={textAreaStyles}
+          />
+          <div className={styles.hastagsWrapper}>
+            <HastagsTextArea
+              onChange={handleChangeHastags}
+              onKeyDown={handleKeyDown}
+              value={HastagValue}
+              darkMode={darkMode}
+              disabled={isTextAreaDisabled}
+            />
+            {hastagOne && (
+              <Hastag content={hastagOne} setHastag={setHastagOne} />
+            )}
+            {hastagTwo && (
+              <Hastag content={hastagTwo} setHastag={setHastagTwo} />
+            )}
+            {hastagThree && (
+              <Hastag content={hastagThree} setHastag={setHastagThree} />
+            )}
+          </div>
           {imgURL && (
             <section className={styles.ImgSection}>
               <button
@@ -138,12 +206,69 @@ export default function ComposeSoftee() {
               <img className={styles.uploadedImg} src={imgURL} />
             </section>
           )}
-          <div className={styles.buttonCont}>
-            <Button disabled={isDisabled}>Softeet</Button>
-          </div>
+          <Button disabled={isDisabled}>Softeet</Button>
         </form>
       </section>
       <Footer />
     </>
+  );
+}
+
+function Hastag({ content, setHastag }) {
+  function handleClick() {
+    setHastag(null);
+  }
+  return (
+    <>
+      <p onClick={handleClick} className={styles.hastag}>
+        #{content}
+        <span className={styles.deleteHastag}>
+          <button>x</button>
+        </span>
+      </p>
+    </>
+  );
+}
+
+function HastagsTextArea({ onChange, onKeyDown, value, darkMode, disabled }) {
+  return (
+    <input
+      disabled={disabled}
+      size={4}
+      maxLength={15}
+      placeholder="#hastags"
+      onChange={onChange}
+      onKeyDown={onKeyDown}
+      className={
+        darkMode
+          ? `${styles.hastagsArea} ${styles.darkMode}`
+          : styles.hastagsArea
+      }
+      value={value}
+    ></input>
+  );
+}
+
+function SoffteeTextArea({
+  onDragEnter,
+  onDragLeave,
+  onDrop,
+  onChange,
+  darkMode,
+  value,
+  textAreaStyles,
+}) {
+  return (
+    <textarea
+      onDragEnter={onDragEnter}
+      onDragLeave={onDragLeave}
+      onDrop={onDrop}
+      onChange={onChange}
+      placeholder="¿Qué esta pasando?"
+      className={
+        darkMode ? `${textAreaStyles} ${styles.darkMode}` : textAreaStyles
+      }
+      value={value}
+    ></textarea>
   );
 }
