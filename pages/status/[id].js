4ba1c2f9a4f6a52dcdc854e351c925e5211afb6b
Manu <manutorres1996@gmail.com>
2022-01-13 22:35:19 +0100

    feat:(Theme Context Creation: Footer)

--- pages/status/[id].js
+++ pages/status/[id].js
@@ -3,16 +3,22 @@ import Softee from "../../components/Softee";
 import Head from "next/head";
 import Footer from "components/Footer";
 import styles from "styles/SingleSoftee.module.css";
+import { useContext } from "react";
+import { ThemeContext } from "hooks/themeContext";
 
 export default function SofteePage({ data }) {
+  const { toggle: darkMode } = useContext(ThemeContext);
   return (
     <>
       <Head>
         <title>Softee / Softter</title>
       </Head>
       <Header location="Softee" />
-      <Softee {...data} />
-      <section className={styles.body}></section>
+      <section
+        className={darkMode ? `${styles.body} ${styles.darkMode}` : styles.body}
+      >
+        <Softee {...data} />
+      </section>
       <Footer />
     </>
   );
