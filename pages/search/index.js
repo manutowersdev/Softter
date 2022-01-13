c6d8a113a51162753e91e577525f5df836267f94
Manu <manutorres1996@gmail.com>
2022-01-13 22:39:29 +0100

    feat:(Theme Context Creation)

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,12 +1,18 @@
 import Footer from "components/Footer";
 import Header from "components/Header";
+import { ThemeContext } from "hooks/themeContext";
+import { useContext } from "react";
 import styles from "styles/Search.module.css";
 
 export default function Search() {
+  const { toggle: darkMode } = useContext(ThemeContext);
+
   return (
     <>
       <Header location="Search" />
-      <section className={styles.body}></section>
+      <section
+        className={darkMode ? `${styles.body} ${styles.darkMode}` : styles.body}
+      ></section>
       <Footer />
     </>
   );
