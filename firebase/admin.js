2d58c791b9f573fa4f99d3d6b9faacc18834fcd0
Manu <manutorres1996@gmail.com>
2022-02-02 18:05:37 +0100

    final2

--- firebase/admin.js
+++ firebase/admin.js
@@ -1,5 +1,6 @@
 const admin = require("firebase-admin")
-const serviceAccount = require("./firebase-keys.json")
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY)
 
 let app
 if (admin.apps.length < 1) {
